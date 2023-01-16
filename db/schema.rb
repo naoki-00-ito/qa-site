d8376cc74b25e83c2cacf9b171bd7c1bdefc30ac
naoki-00-ito <na.o.ki.00.i.to@gmail.com>
2023-01-16 20:30:21 +0900

    Update: db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_16_105744) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_16_112627) do
   create_table "ansers", force: :cascade do |t|
     t.string "name"
     t.text "content"
@@ -18,6 +18,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_16_105744) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["question_id"], name: "index_ansers_on_question_id"
+  end
+
+  create_table "answers", force: :cascade do |t|
+    t.string "name"
+    t.text "content"
+    t.integer "question_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
   end
 
   create_table "questions", force: :cascade do |t|
@@ -29,4 +38,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_16_105744) do
   end
 
   add_foreign_key "ansers", "questions"
+  add_foreign_key "answers", "questions"
 end
